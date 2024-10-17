bc5614fc3c543c4fbed226b94b11b1e3c991fe79
mharoon-dev <haroon.aghani@gmail.com>
2024-10-17 18:08:40 +0500

    add code

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "../../frontend/src/Redux/store.js";
+import store from "./Redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
